d81bb8861ffa38a70e8caeab828059e1305d89d0
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-14 17:48:19 +0300

    Edited index.ts for cors

--- index.ts
+++ index.ts
@@ -4,6 +4,16 @@ import path from 'path';
 const app: Express = express();
 const PORT = 8080;
 
+// Cors
+import cors from 'cors';
+const corsOptions = require('./config/corsOptions');
+
+// Middlewares
+const credentials = require('./middleware/credentials');
+
+app.use(credentials);
+app.use(cors(corsOptions));
+
 app.use(express.json());
 
 app.use(express.urlencoded({ extended: false }));
@@ -14,14 +24,12 @@ const authRoutes = require('./routes/auth')
 
 app.use('/oauth', authRoutes);
 
-
 app.get('/', (req: Request, res: Response) => {
     res.send('Express + Typesciprt Server')
 });
 
 
 const startUp = async () => {
-
     app.listen(PORT, () => {
         console.log('started at ' + PORT);
     })
