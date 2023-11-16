14e1273561489b1848a35d3ea1b372cc06b6ac8a
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-16 23:58:22 +0300

    Edited config.ts for mongodb info

--- config/config.ts
+++ config/config.ts
@@ -1,10 +1,20 @@
 const config: {
+    db: {
+        password: string
+        username: string
+        database: string
+    },
     redis: {
         password: string
         host: string
         port: number
     }
 } = {
+    db: {
+        password: '',
+        username: '',
+        database: 'letgo_clone'
+    },
     redis: {
         password: '',
         host: '',
