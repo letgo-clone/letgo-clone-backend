10ba26e984d9ffd475fcc1a741ff816c2766581f
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-29 16:01:04 +0300

    Edited getActualAdvert func for postgresql

--- controllers/advert.ts
+++ controllers/advert.ts
@@ -1,11 +1,16 @@
 import express, { Request, Response, NextFunction } from 'express';
 const CustomError = require('../errors/CustomError');
 
-const {advertdata} = require('../models/dummy');
+const pool = require('../helpers/postgre');
 
 exports.getActualAdvert = async function (req: Request, res: Response, next: NextFunction) {
     try {
-        return res.status(200).json(advertdata.data);
+        const data = await pool.query(
+            "SELECT ad.id, ad.title, to_char(ad.created_at,'DD Month') as date, ad.description, ad.images, ad.price, ad.parameters, u.user_type, ads.display_type, ads.display_name, cy.city, ct.county FROM adverts ad LEFT JOIN users u ON ad.user_id = u.id LEFT JOIN advert_status ads ON ads.id = ad.status_id LEFT JOIN cities cy ON cy.id = ad.city_id LEFT JOIN counties ct ON ct.id = ad.county_id WHERE ad.is_deleted = FALSE AND ad.is_verify = TRUE AND u.is_deleted = FALSE AND ads.is_visible = TRUE"
+        );
+        const user = data.rows;
+
+        return res.status(200).json(user);
 
     } catch(err) {
         next(err); 
