4624236c3e872629743f5b9cc396166b86b13a9d
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-30 13:23:55 +0300

    Created Post advert api

--- controllers/advert.ts
+++ controllers/advert.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 const CustomError = require('../errors/CustomError');
 
 const pool = require('../helpers/postgre');
+const redis = require('../helpers/redis');
 
 exports.getActualAdvert = async function (req: Request, res: Response, next: NextFunction) {
     try {
@@ -34,5 +35,63 @@ exports.getAdvertDetail = async function (req: Request, res: Response, next: Nex
 
     } catch(err) {
         next(err); 
+    }
+}
+
+exports.postAdvert = async function (req: Request, res: Response, next: NextFunction) {
+    const getRedisData = await redis.RedisClient.get('currentUser');
+    const parseUser = JSON.parse(getRedisData);
+
+    const user_id = parseUser.user_id;
+    const title = req.body.title;
+    const description = req.body.description;
+    const images = req.body.images;
+    const parameters = req.body.parameters;
+    const price = req.body.price;
+    const city_id = req.body.city_id;
+    const county_id = req.body.county_id;
+
+    try {
+        if (!user_id || user_id == '') {
+            throw new CustomError(403, "user_id alanını belirtmelisiniz.");
+        }
+
+        if (!title || title == '') {
+            throw new CustomError(400, "title alanını belirtmelisiniz.");
+        }
+
+        if (!description || description == '') {
+            throw new CustomError(400, "description alanını belirtmelisiniz.");
+        }
+
+        if (!images) {
+            throw new CustomError(400, "images alanını belirtmelisiniz.");
+        }
+
+        if (!parameters) {
+            throw new CustomError(400, "parameters alanını belirtmelisiniz.");
+        }
+
+        if (!price || price == '') {
+            throw new CustomError(400, "price alanını belirtmelisiniz.");
+        }
+
+        if (!city_id || city_id == '') {
+            throw new CustomError(400, "city_id alanını belirtmelisiniz.");
+        }
+
+        if (!county_id || county_id == '') {
+            throw new CustomError(400, "county_id alanını belirtmelisiniz.");
+        }
+
+        const insertQuery = 'INSERT INTO adverts(title, description, user_id, images, parameters, price, city_id, county_id) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *';
+        const values = [title, description, user_id, JSON.stringify(images), JSON.stringify(parameters), price, city_id, county_id];
+
+        const status = await pool.query(insertQuery, values);
+        const advert_id = status.rows[0].id;
+       
+        return res.status(201).json({ 'success': 'true' , 'advert_id' : advert_id})
+    }catch (err){
+        next(err)
     }
 }
--- routes/advert.ts
+++ routes/advert.ts
@@ -4,7 +4,11 @@ const router = express.Router();
 const advertController = require("../controllers/advert");
 const multer = require('../helpers/multer');
 
+const verifyJWT = require('../middleware/verifyJWT');
+
 router.get("/actual", advertController.getActualAdvert);
 router.get("/actual/:detail", advertController.getAdvertDetail);
+
+router.post('/actual', verifyJWT, multer.image_upload.single('photo'), advertController.postAdvert, multer.body_parse.any())
 
 module.exports = router;
