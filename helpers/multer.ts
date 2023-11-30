bda5150c2de5d835a5b07cd1fd9d4dd88f3fc87f
Fırat Yıldız <firatyildiznet@gmail.com>
2023-11-30 12:15:09 +0300

    Added image upload feature in multer

--- helpers/multer.ts
+++ helpers/multer.ts
@@ -1,9 +1,11 @@
 const multer = require('multer');
 
 const storage = multer.memoryStorage();
+const image_upload = multer({storage});
 
 const body_parse = multer();
 
 module.exports = {
-    body_parse
+    body_parse,
+    image_upload
 }
