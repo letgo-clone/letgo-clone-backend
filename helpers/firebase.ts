6a73a9cfb34908039b5428b6db5cbaed7a84188b
Fırat Yıldız <firatyildiznet@gmail.com>
2023-12-03 00:39:07 +0300

    Created firebase helper

---
+++ helpers/firebase.ts
@@ -0,0 +1,13 @@
+const admin = require('firebase-admin');
+const serviceAccount = require('../config/firebase-adminsdk.json');
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    storageBucket: "letgo-clone-3ddcb.appspot.com"
+})
+
+const bucket = admin.storage().bucket()
+
+module.exports = {
+  bucket
+}
