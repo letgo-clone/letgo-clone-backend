2daacfb76046945e92d669398de53cfc82607954
Fırat Yıldız <firatyildiznet@gmail.com>
2024-01-15 10:41:49 +0300

    Revert "Removed sharp in uploadImage"
    This reverts commit edd94696a6d2c0fd34a6ed81805b0b4ab69b4a88.

--- helpers/uploadImage.ts
+++ helpers/uploadImage.ts
@@ -1,9 +1,12 @@
+const sharp = require('sharp');
 const firebase = require('./firebase'); // Firebase konfigürasyonu buradan alınabilir
 
 async function uploadMultipleImages(files: any[], folder: string, who: string, deleteIfExists: string[] = []) {
     try {
         const uploadPromises = files.map(async (file, index) => {
-            const filePath = 'images/' + folder + ((file.originalname).replace(' ', '-')).substr(0, 6) + '-' + who + '-' + Date.now();
+            const compressedBuffer = await sharp(file.buffer).toBuffer();
+            const filePath = 'images/' + folder + ((file.originalname).replace(' ', '-')).substr(0,6) + '-' + who + '-' + + Date.now();
+
 
             if (deleteIfExists[index]) {
                 const existingFile = firebase.bucket.file(deleteIfExists[index]);
@@ -20,22 +23,28 @@ async function uploadMultipleImages(files: any[], folder: string, who: string, d
                     contentType: file.mimetype
                 }
             });
+            const imageInfo = await sharp(compressedBuffer).metadata();
 
             return new Promise((resolve, reject) => {
                 blobWriter.on('error', (err: Error) => {
                     reject(err);
                 });
 
-                blobWriter.on('finish', async () => {
-                    const signedURL = await blob.getSignedUrl({ action: 'read', expires: '03-01-2500' });
-
-                    resolve({
-                        url: signedURL[0],
-                        path: filePath
+                blobWriter.on('finish', () => {
+                    blob.getSignedUrl({ action: 'read', expires: '03-01-2500' }, (err: Error, signedURL: any) => {
+                        if (err) {
+                            reject(err);
+                        }
+                        resolve({ 
+                            url: signedURL, 
+                            path: filePath,
+                            width: imageInfo.width,
+                            height: imageInfo.height
+                        });
                     });
                 });
 
-                blobWriter.end(file.buffer);
+                blobWriter.end(compressedBuffer);
             });
         });
 
@@ -44,6 +53,7 @@ async function uploadMultipleImages(files: any[], folder: string, who: string, d
         throw error;
     }
 }
+
 
 async function removeOldImage(deleteIfExist = '') {
     try {
